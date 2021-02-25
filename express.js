9fd0985f7f723b0113cfba4edc286ea3cd892bae
karanvirdhillon93 <karanvir.dhillon30@online.camosun.ca>
2021-02-25 21:31:53 +0000

    initial commit

---
+++ express.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import morgan from 'morgan';
+import cors from 'cors';
+const app = express();
+// Express middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(compression());
+app.use(morgan('dev'));
+app.use(cors());
+// Routing
+app.get('/', (req, res) => {
+ res.send('Node.js Server is live!');
+});
+export default app;
