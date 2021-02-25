d15fa9765f44df3ab87a680a9f8f0f5aab557065
karanvirdhillon93 <karanvir.dhillon30@online.camosun.ca>
2021-02-25 21:35:02 +0000

    added default path for API

--- express.js
+++ express.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import morgan from 'morgan';
 import cors from 'cors';
+import apiRouter from './routes/api_router.js';
 const app = express();
 // Express middleware
 app.use(bodyParser.json());
@@ -16,4 +17,5 @@ app.use(cors());
 app.get('/', (req, res) => {
  res.send('Node.js Server is live!');
 });
+app.use('/api', apiRouter);
 export default app;
