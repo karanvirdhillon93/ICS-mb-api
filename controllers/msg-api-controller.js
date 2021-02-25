816e3ba37ec19d69054f25fb2636c2eaaf22b6a0
karanvirdhillon93 <karanvir.dhillon30@online.camosun.ca>
2021-02-25 21:38:37 +0000

    add controller for /messages endpoint

---
+++ controllers/msg-api-controller.js
@@ -0,0 +1,10 @@
+// GET Request Handler
+const getAllMessages = (req, res) => {
+    res.status(200).send('Successful API GET Request');
+   };
+   // POST Request Handler
+   const addNewMessage = async (req, res) => {
+    res.status(200).send('Successful API POST Request');
+   };
+   export default { getAllMessages, addNewMessage };
+   
