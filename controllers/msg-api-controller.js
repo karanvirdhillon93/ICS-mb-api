a6335d33ebae588d864403d61e3078f27fa0363c
karanvirdhillon93 <karanvir.dhillon30@online.camosun.ca>
2021-02-26 03:48:21 +0000

    messages array

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -1,6 +1,8 @@
+import messages from './msg-api-controller.js'
 // GET Request Handler
 const getAllMessages = (req, res) => {
-    res.status(200).send('Successful API GET Request');
+   // res.status(200).send('Successful API GET Request');
+   res.status(200).json(messages);
    };
    // POST Request Handler
    const addNewMessage = async (req, res) => {
--- msg-api-controller.js
+++ msg-api-controller.js
@@ -1,8 +1,9 @@
-var message=([
+var messages=([
     {id:0, name:"Bill",msgText:"Hi All!"},
     {id:1, name:"Ann",msgText:"ICS 211 is fun!"},
     {id:2, name:"Johnny",msgText:"I'm stranded !"},
     {id:3, name:"Barb",msgText:"Hi"},
     {id:4, name:"Frank",msgText:"Who's tired?"},
     {id:5, name:"Sarah",msgText:"I heart React"}
-  ]);
+]);
+  export default messages;
