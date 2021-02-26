d01195bdd13f184e2e8f2b4d40589799fffa973e
karanvirdhillon93 <karanvir.dhillon30@online.camosun.ca>
2021-02-26 03:50:41 +0000

    Fixed bug with messages array

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -1,4 +1,4 @@
-import messages from './msg-api-controller.js'
+import messages from '/home/student/ics221-mb-api/msg-api-controller.js'
 // GET Request Handler
 const getAllMessages = (req, res) => {
    // res.status(200).send('Successful API GET Request');
