4eee289ddda296002fd430556d3d7afb7a315b74
karanvirdhillon93 <karanvir.dhillon30@online.camosun.ca>
2021-02-26 03:54:03 +0000

    - Implementing the getAllMessages() GET Request Handler

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -2,7 +2,13 @@ import messages from '/home/student/ics221-mb-api/msg-api-controller.js'
 // GET Request Handler
 const getAllMessages = (req, res) => {
    // res.status(200).send('Successful API GET Request');
-   res.status(200).json(messages);
+   try {
+    res.status(200).json(messages);
+   } catch (err) {
+    res.status(400).send('Bad Request');
+   }
+   
+ //  res.status(200).json(messages);
    };
    // POST Request Handler
    const addNewMessage = async (req, res) => {
