18d17715109d06fb0d77b323a26d01204d606ecd
karanvirdhillon93 <karanvir.dhillon30@online.camosun.ca>
2021-02-26 03:21:41 +0000

    added messages global variable

---
+++ msg-api-controller.js
@@ -0,0 +1,8 @@
+var message=([
+    {id:0, name:"Bill",msgText:"Hi All!"},
+    {id:1, name:"Ann",msgText:"ICS 211 is fun!"},
+    {id:2, name:"Johnny",msgText:"I'm stranded !"},
+    {id:3, name:"Barb",msgText:"Hi"},
+    {id:4, name:"Frank",msgText:"Who's tired?"},
+    {id:5, name:"Sarah",msgText:"I heart React"}
+  ]);
