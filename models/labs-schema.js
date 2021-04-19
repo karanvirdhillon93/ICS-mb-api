6ef47de1c55ac12aa03fba1ce5ea04bc5114d935
Karanvir Dhillon <C0373297@online.camosun.ca>
2021-04-19 17:53:28 +0000

    Coded the labs-schema

---
+++ models/labs-schema.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const labsSchema = new mongoose.Schema({
+    labNo: {
+        type: Number,
+        required: true,
+        },
+    topic: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 80,
+        match: /[A-Za-z]/
+    }
+});
+
+labsSchema.set('toJSON', {
+    versionKey: false,
+    virtuals: true,
+    transform: (doc, ret) => { delete ret._id; }
+   });   
+   export default mongoose.model('labs', labsSchema);
+   //export default messageSchema;
